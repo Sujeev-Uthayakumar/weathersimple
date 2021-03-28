1dbe423ca81fdfcdc05f52a7d0b00b7b77377358
Sujeev-Uthayakumar <76674104+Sujeev-Uthayakumar@users.noreply.github.com>
2021-03-28 02:01:33 -0400

    master

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-
+let weather = {
+    apiKey: "e84218af7c3e6ca70309d48ea060952e",
+    fetchWeather: function (city) {
+        fetch("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+            .then((response) => response.json())
+            .then((data) => console.log(data))
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather;
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+    }
+}
 
 
